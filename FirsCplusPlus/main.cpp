036d54576f6d8aa38653b5d71919caa32d3ef770
shujaetdin <shujaetdin@gmail.com>
2018-01-23 08:36:18 -0700

    Learned about some cool stuff

---
+++ FirsCplusPlus/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  FirsCplusPlus
+//
+//  Created by Din, Shujaet on 1/23/18.
+//  Copyright © 2018 Din, Shujaet. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+ {
+    // insert code here...
+     cout << "Hello, World!\n" << endl;
+    return 0;
+}
